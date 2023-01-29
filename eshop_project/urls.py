00491c2a708d67b66e84ff6535bc188457953c61
Mahdi_f__P <mhdyflah78@gmail.com>
2023-01-29 18:20:54 +0330

    main folder

---
+++ eshop_project/urls.py
@@ -0,0 +1,25 @@
+"""eshop_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the includes() function: from django.urls import includes, path
+    2. Add a URL to urlpatterns:  path('blog/', includes('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("", include("home_module.urls")),
+    path('app-api/', include("api_app.urls")),
+    path('contact-us/', include("contact_us.urls")),
+    path("products/", include("product_module.urls")),
+    path("admin/", admin.site.urls),
+]
