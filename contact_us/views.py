6ef36a9fecd255875212cc624fb68d0722f12d7d
Mahdi_f__P <mhdyflah78@gmail.com>
2023-01-30 16:18:19 +0330

    add FormView in class based view and edited ContactUsView

--- contact_us/views.py
+++ contact_us/views.py
@@ -1,27 +1,23 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-from django.views import View
+from django.views.generic.edit import FormView
 
 from .forms import ContactUsModelForm
 
 
-class ContactUsView(View):
-    def get(self, request):
-        form = ContactUsModelForm()
+class ContactUsView(FormView):
+    template_name = "contact_us/contact_us_page.html"
+    form_class = ContactUsModelForm
 
-        return render(request, "contact_us/contact_us_page.html", context={
-            'form': form
-        })
-
-    def post(self, request):
-        data = request.POST
-        form = ContactUsModelForm(data=data)
-
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('home_page'))
-
-        return render(request, "contact_us/contact_us_page.html", context={
-            'form': form
-        })
+    # def post(self, request):
+    #     data = request.POST
+    #     form = ContactUsModelForm(data=data)
+    #
+    #     if form.is_valid():
+    #         form.save()
+    #         return HttpResponseRedirect(reverse('home_page'))
+    #
+    #     return render(request, "contact_us/contact_us_page.html", context={
+    #         'form': form
+    #     })
