e84b48cec386633e5cfb76a97d4db8c35f211ace
Mahdi_f__P <mhdyflah78@gmail.com>
2023-01-30 14:36:09 +0330

    comment get_queryset function in views.py/ ProductListView class

--- product_module/views.py
+++ product_module/views.py
@@ -11,10 +11,10 @@ class ProductListView(ListView):
     model = Product
     context_object_name = "products"
 
-    def get_queryset(self):
-        base_query = super(ProductListView, self).get_queryset()
-        active_products = base_query.filter(is_active=True)
-        return active_products
+    # def get_queryset(self):
+    #     base_query = super(ProductListView, self).get_queryset()
+    #     active_products = base_query.filter(is_active=True)
+    #     return active_products
 
 
 class ProductDetailView(TemplateView):
