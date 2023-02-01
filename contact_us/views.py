a5d65267415a64eceabfb5eb5f5ac3f43fb89ea3
Mahdi_f__P <mhdyflah78@gmail.com>
2023-02-01 20:02:03 +0330

    Reformated contact_us/views.py

--- contact_us/views.py
+++ contact_us/views.py
@@ -1,10 +1,8 @@
 from django.views.generic.edit import CreateView
 from .forms import ContactUsModelForm
-from .models import ContactUs
 
 
 class ContactUsView(CreateView):
-    model = ContactUs
     form_class = ContactUsModelForm
     template_name = "contact_us/contact_us_page.html"
     success_url = "/"
