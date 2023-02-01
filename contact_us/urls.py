bab87e2527bb50c4e59eccaf29ed4a852c512953
Mahdi_f__P <mhdyflah78@gmail.com>
2023-02-01 20:37:27 +0330

    create class based view in views.py and set url in urls.py

--- contact_us/urls.py
+++ contact_us/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ContactUsView.as_view(), name="contact-us-page"),
+    path('profile/', views.CreateProfileView.as_view(), name="create-profile-page"),
+
 ]
--- contact_us/views.py
+++ contact_us/views.py
@@ -1,4 +1,6 @@
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView
+from django.views import View
 from .forms import ContactUsModelForm
 
 
@@ -6,3 +8,12 @@ class ContactUsView(CreateView):
     form_class = ContactUsModelForm
     template_name = "contact_us/contact_us_page.html"
     success_url = "/"
+
+
+class CreateProfileView(View):
+    def get(self, request):
+        return render(request, 'contact_us/create_profile.html', context={})
+
+    def post(self, request):
+        print(request.FILES)
+        return redirect('/contact-us/profile')
