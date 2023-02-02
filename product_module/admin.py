591b79f403817ba536617383ce4bf4f23c77e63b
Mahdi_f__P <mhdyflah78@gmail.com>
2023-02-03 00:33:29 +0330

    add ModelAdmin with name : ProductCategoryAdmin and set to register

--- product_module/admin.py
+++ product_module/admin.py
@@ -10,8 +10,33 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ['price', 'is_active']
 
 
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'is_active',
+        'is_delete'
+    ]
+
+    list_editable = [
+        'is_active',
+        'is_delete'
+    ]
+
+    list_filter = [
+        'is_active',
+        'is_delete'
+    ]
+
+
+class ProductTagAdmin(admin.ModelAdmin):
+    pass
+
+
+class ProductBrandAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(models.Product, ProductAdmin)
-admin.site.register(models.ProductCategory)
+admin.site.register(models.ProductCategory, ProductCategoryAdmin)
 admin.site.register(models.ProductTag)
 admin.site.register(models.ProductBrand)
-
