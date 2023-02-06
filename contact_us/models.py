e81134df18cf9507230c8e1596f0bfe79c4cc5c7
Mahdi_f__P <mhdyflah78@gmail.com>
2023-02-06 18:28:30 +0330

    add verbose_name in UserProfile in field image & create migrations file

--- contact_us/models.py
+++ contact_us/models.py
@@ -22,4 +22,4 @@ class ContactUs(models.Model):
 
 
 class UserProfile(models.Model):
-    image = models.FileField(upload_to="images")
+    image = models.FileField(upload_to="images", verbose_name="عکس شما")
