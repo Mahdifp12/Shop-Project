6c019b174d342ac6751e8aebec0edd0c320434e6
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-02-09 18:15:14 +0330

    add session for product favorite

--- product_module/urls.py
+++ product_module/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='product-list'),
-    path('<slug:slug>', views.ProductDetailView.as_view(), name='product-detail')
+    path('product-favorite', views.ProductFavorite.as_view(), name='product-favorite'),
+    path('<slug:slug>', views.ProductDetailView.as_view(), name='product-detail'),
 ]
--- product_module/views.py
+++ product_module/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import redirect
+from django.views import View
 from django.views.generic import ListView, DetailView
 
 from .models import Product
@@ -12,3 +14,11 @@ class ProductListView(ListView):
 class ProductDetailView(DetailView):
     template_name = "product_module/product_detail.html"
     model = Product
+
+
+class ProductFavorite(View):
+    def post(self, request):
+        product_id = request.POST["product_id"]
+        product = Product.objects.get(pk=product_id)
+        request.session["product_favorite"] = product.id
+        return redirect(product.get_absolute_url())
