45c2975316b583d30623d3ee4458b3e7f90eb297
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-02-21 13:18:53 +0330

    add login form in account_app

--- account_app/views.py
+++ account_app/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
-from django.contrib.auth import get_user_model
+from django.views import View
+
+from .forms import RegisterForm
+
+
 # Create your views here.
 
-user = get_user_model()
+class RegisterView(View):
+    def get(self, request):
+        register_form = RegisterForm()
+        context = {
+            "register_form": register_form
+        }
+
+        return render(request, 'account_app/register.html', context)
+
+    def post(self, request):
+        register_form = RegisterForm(request.POST or None)
+
+        if register_form.is_valid():
+            print(register_form.cleaned_data)
+
+        context = {
+            "register_form": register_form
+        }
+
+        return render(request, 'account_app/register.html', context)
