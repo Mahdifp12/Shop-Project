88fa2d970fc9da07691775aec1f64b34a4bb521e
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-02-22 12:58:35 +0330

    reformated settings.py with PEP

--- eshop_project/settings.py
+++ eshop_project/settings.py
@@ -62,8 +62,10 @@ ROOT_URLCONF = "eshop_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
-        ,
+        "DIRS": [
+            BASE_DIR / 'templates',
+        ],
+
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
