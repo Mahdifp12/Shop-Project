651517bfa0a2fd7bc0a41b44ae4444a20b5bc794
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-03-18 19:31:47 +0330

    refactor or change views name in urls file

--- account_app/urls.py
+++ account_app/urls.py
@@ -4,8 +4,9 @@ from . import views
 urlpatterns = [
     path('register', views.RegisterView.as_view(), name="register-page"),
     path('login', views.LoginView.as_view(), name="login-page"),
-    path('forget-password', views.ForgetPassword.as_view(), name="forgot-password-page"),
-    path('reset-password/<active_code>', views.ResetPassword.as_view(), name="reset-password-page"),
-    path('activate-account/<email_active_code>', views.ActivateAccount.as_view(), name="activate-account")
+    path('logout', views.LogoutView.as_view(), name="logout-page"),
+    path('forget-password', views.ForgetPasswordView.as_view(), name="forgot-password-page"),
+    path('reset-password/<active_code>', views.ResetPasswordView.as_view(), name="reset-password-page"),
+    path('activate-account/<email_active_code>', views.ActivateAccountView.as_view(), name="activate-account")
 
 ]
