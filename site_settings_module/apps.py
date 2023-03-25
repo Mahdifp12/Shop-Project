cf88de78f9eda0a93306230e50e4272e6ab2a574
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-03-25 12:27:21 +0330

    add verbose name in apps.py

--- site_settings_module/apps.py
+++ site_settings_module/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SiteSettingsModuleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'site_settings_module'
+    verbose_name = "ماژول تنظیمات وبسایت"
