61b07b3075bd33c476ad97b264a2a5282509076f
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-03-30 01:34:03 +0330

    add About view (class based)

--- home_module/views.py
+++ home_module/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from site_settings_module.models import SiteSettings
+from site_settings_module.models import SiteSettings, FooterLink, FooterLinksBox
 
 
 class HomePageView(TemplateView):
@@ -22,11 +22,21 @@ def site_header_component(request):
 
 def site_footer_component(request):
     settings: SiteSettings = SiteSettings.objects.filter(is_main_setting=True).first()
-
+    footer_link_boxes = FooterLinksBox.objects.all()
     return render(
         request,
         template_name="shared/site_footer_component.html",
         context={
-            "settings": settings
+            "settings": settings,
+            "footer_link_boxes": footer_link_boxes
         }
     )
+
+
+class AboutView(TemplateView):
+    template_name = "home_module/about_page.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["settings"]: SiteSettings = SiteSettings.objects.filter(is_main_setting=True).first()
+        return context
