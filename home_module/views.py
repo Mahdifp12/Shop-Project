ae01e94f427b4e381b167a00d65a9251247a1a2e
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-03-29 23:57:02 +0330

    add data context with sitesettings model in footer component function

--- home_module/views.py
+++ home_module/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from site_settings_module.models import SiteSettings
 
+
 class HomePageView(TemplateView):
     template_name = 'home_module/index_page.html'
 
@@ -20,8 +21,12 @@ def site_header_component(request):
 
 
 def site_footer_component(request):
+    settings: SiteSettings = SiteSettings.objects.filter(is_main_setting=True).first()
+
     return render(
         request,
         template_name="shared/site_footer_component.html",
-        context={}
+        context={
+            "settings": settings
+        }
     )
