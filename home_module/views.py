14d93b26fa658d02b4430b13c85e3f1c9a1784cb
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-03-29 23:10:26 +0330

    change site header component function with get site settings module

--- home_module/views.py
+++ home_module/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+from site_settings_module.models import SiteSettings
 
 class HomePageView(TemplateView):
     template_name = 'home_module/index_page.html'
@@ -12,11 +12,11 @@ class HomePageView(TemplateView):
 
 
 def site_header_component(request):
-    return render(
-        request,
-        template_name="shared/site_header_component.html",
-        context={}
-    )
+    settings: SiteSettings = SiteSettings.objects.filter(is_main_setting=True).first()
+
+    return render(request, template_name="shared/site_header_component.html", context={
+        "settings": settings
+    })
 
 
 def site_footer_component(request):
