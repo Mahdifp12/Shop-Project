82ba54490e406279b9116dd2e4697f8bd2f420a3
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-04-01 14:36:21 +0330

    add slider admin class and add to register slider

--- site_settings_module/admin.py
+++ site_settings_module/admin.py
@@ -6,9 +6,12 @@ class FooterLinkAdmin(admin.ModelAdmin):
     list_display = ['title', 'url']
 
 
-# Todo : I must add a new class for SliderAdmin ..!
+class SliderAdmin(admin.ModelAdmin):
+    list_display = ["title", "is_active", "url"]
+    list_editable = ["is_active", "url"]
+
 
 admin.site.register(models.SiteSettings)
 admin.site.register(models.FooterLinksBox)
 admin.site.register(models.FooterLink, FooterLinkAdmin)
-admin.site.register(models.Slider)
+admin.site.register(models.Slider, SliderAdmin)
