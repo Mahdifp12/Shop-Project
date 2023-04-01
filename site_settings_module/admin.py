00ef548c55f81fa63596178c2c03f5ded34941d2
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-04-01 13:42:48 +0330

    add register for slider model in panel admin & add todo for futrue

--- site_settings_module/admin.py
+++ site_settings_module/admin.py
@@ -6,6 +6,9 @@ class FooterLinkAdmin(admin.ModelAdmin):
     list_display = ['title', 'url']
 
 
+# Todo : I must add a new class for SliderAdmin ..!
+
 admin.site.register(models.SiteSettings)
 admin.site.register(models.FooterLinksBox)
 admin.site.register(models.FooterLink, FooterLinkAdmin)
+admin.site.register(models.Slider)
