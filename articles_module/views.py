9d32cb06e91c2f73251b6a7bbeeff7321db7704d
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-04-08 12:27:37 +0330

    NEW & MODIFIED : create a new ListView (class based view) change from normal article view

--- articles_module/views.py
+++ articles_module/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView
+from .models import Article
 
 
 # Create your views here.
 
-class ArticlesView(View):
-    def get(self, request):
-        return render(request, template_name="", context={})
+class ArticlesView(ListView):
+    model = Article
+    paginate_by = 4
+    template_name = "articles_module/articles_page.html"
