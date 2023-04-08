cc58bd46237c8c2cf0db456c9c71627776b41b62
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-04-08 11:30:44 +0330

    REFACTOR : add a new class based view for articles

--- articles_module/views.py
+++ articles_module/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+
+class ArticlesView(View):
+    def get(self, request):
+        return render(request, template_name="", context={})
