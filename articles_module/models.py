23358314dfe104bc05f56e948a7e6b676a476c6f
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-04-14 18:49:20 +0330

    REFACTOR: i'm changed get_full_name with if and else logical

--- articles_module/models.py
+++ articles_module/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from account_app.models import User
+
 
 # Create your models here.
 
@@ -25,6 +27,7 @@ class Article(models.Model):
     text = models.TextField(verbose_name="متن مقاله")
     is_active = models.BooleanField(default=True, verbose_name="فعال / غیرفعال")
     selected_categories = models.ManyToManyField(ArticleCategory, verbose_name="دسته بندی ها", )
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="نویسنده", null=True, editable=False)
 
     class Meta:
         verbose_name = "مقاله"
