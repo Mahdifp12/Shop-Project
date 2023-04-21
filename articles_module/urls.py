62740ee71acf993f05561618dab7a5cf7b180b09
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-04-21 13:12:04 +0330

    ADD & CHANGE : i'm added a new url path for categories filter

--- articles_module/urls.py
+++ articles_module/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.ArticlesView.as_view(), name="article-list")
+    path('', views.ArticlesView.as_view(), name="article-list"),
+    path('cat/<str:category>', views.ArticlesView.as_view(), name="articles-by-category-list")
 ]
