ccca14afa060146edb8f94cb46e52b7d101ddc97
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-04-24 15:11:09 +0330

    ADD & REFACTOR : i'm added a new field for about_user & refactor and fixing user avatar

--- account_app/models.py
+++ account_app/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 class User(AbstractUser):
     email_active_code = models.CharField(max_length=100, verbose_name="کد فعالسازی ایمیل")
-    avatar = models.CharField(max_length=20, verbose_name="تصویر آواتار", null=True, blank=True)
+    avatar = models.ImageField(upload_to="images/users_avatar", verbose_name="تصویر آواتار", null=True, blank=True)
+    about_user = models.TextField(null=True, blank=True, verbose_name="درباره شخص")
 
     class Meta:
         verbose_name = "کاربر"
