fc3a09f62dbb4de88bf9c55ec8561ecc9a1e419b
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-05-04 17:58:46 +0330

    ADD & CHANGE : i'm get with jquery for comment text and etc for test then i'm response log

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,9 +2,11 @@ function sendArticleComment() {
     // console.log("submit article comment");
     var comment = $('#commentText').val();
 
-    $.get("articles/add-article-comment", {
-        ArticleComment : comment
+    $.get("/articles/add-article-comment", {
+        ArticleComment : comment,
+        ArticleId : 9,
+        ArticleParent : null
     }).then(res => {
         console.log(res);
-    })
+    });
 }
