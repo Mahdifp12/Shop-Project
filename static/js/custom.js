af25c36402c22c1042baf7292d60c1f9de30c327
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-05-04 20:02:05 +0330

    CHANGE & REFACTOR : i'm refactored this file

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,6 +1,5 @@
 function sendArticleComment() {
-    // console.log("submit article comment");
-    var comment = $('#commentText').val();
+    var comment = $("#commentText").val();
 
     $.get("/articles/add-article-comment", {
         ArticleComment : comment,
