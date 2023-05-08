27ff3224ce695c395e0b7f0e58e3d11d4c4ba877
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-05-08 17:13:48 +0330

    REFACTOR & CHANGE : i'm changed with pep

--- articles_module/views.py
+++ articles_module/views.py
@@ -37,7 +37,7 @@ class ArticleDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super(ArticleDetailView, self).get_context_data()
         article: Article = kwargs.get('object')
-        context['comments']: ArticleComment = ArticleComment.objects.filter(article_id=article.id, parent=None)\
+        context['comments']: ArticleComment = ArticleComment.objects.filter(article_id=article.id, parent=None) \
             .order_by('-create_date') \
             .prefetch_related("articlecomment_set")
         return context
@@ -58,9 +58,13 @@ def add_article_comment(request: HttpRequest):
         article_id = request.GET.get("article_id")
         article_comment = request.GET.get("article_comment")
         parent_id = request.GET.get("parent_id")
-        print(article_id, article_comment, parent_id)
 
-        new_comment = ArticleComment(article_id=article_id, text=article_comment, user_id=request.user.id)
+        new_comment = ArticleComment(
+            article_id=article_id,
+            text=article_comment,
+            user_id=request.user.id,
+            parent_id=parent_id)
+
         new_comment.save()
 
     return HttpResponse("response")
