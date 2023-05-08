fdeadb666997d568a0745269f37773d799625a4f
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-05-08 17:13:03 +0330

    ADD & CHANGE : i'm created a new function for fill parent id with input hidden & dynamicated parent id in sendArticleComment and get that

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,11 +1,17 @@
 function sendArticleComment(articleId) {
     var comment = $("#commentText").val();
+    var parentId = $("#parent_id").val()
 
     $.get("/articles/add-article-comment", {
         article_comment : comment,
         article_id: articleId,
-        parent_id : null
+        parent_id : parentId
     }).then(res => {
         console.log(res);
     });
+}
+
+function fillParentId(parentId){
+    $("#parent_id").val(parentId)
+    document.getElementById('comment_form').scrollIntoView({behavior: "smooth"});
 }
