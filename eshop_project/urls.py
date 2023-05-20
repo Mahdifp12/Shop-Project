bc95031bf540b27d975dfc8dd384fd4be3a55aa1
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-05-20 15:11:29 +0330

    CHANGE & ADD : i'm added a new url path for user panel

--- eshop_project/urls.py
+++ eshop_project/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('api/', include("api_app.urls")),
     path('contact-us/', include("contact_us.urls")),
     path("products/", include("product_module.urls")),
+    path("user/", include("user_panel_module.urls")),
     path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
