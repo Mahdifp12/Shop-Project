dad6f7205a648a96514ac565fde218023cbf4cd2
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-05-24 15:08:52 +0330

    CHANGE & ADD : i'm added a new class based view for edit user profile

--- user_panel_module/views.py
+++ user_panel_module/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from django.views import View
 from django.views.generic import TemplateView
 
 
@@ -7,6 +8,14 @@ from django.views.generic import TemplateView
 
 class UserPanelDashboardPage(TemplateView):
     template_name = "user_panel_module/user_panel_page.html"
+
+
+class EditUserProfilePage(View):
+    def get(self, request: HttpRequest):
+        return render(request, '', {})
+
+    def post(self, request: HttpRequest):
+        pass
 
 
 def user_panel_menu_component(request: HttpRequest):
