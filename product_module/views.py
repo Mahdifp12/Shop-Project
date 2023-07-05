f539753397a14d4a641e9ed9ba65fdcd0234449f
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-07-05 22:00:03 +0330

    ADD & CHANGE : i'm added a new function for components HTML (render_partial) & i'm added a new method with set_queryset for get product query

--- product_module/views.py
+++ product_module/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import redirect
+from django.http import HttpRequest
+from django.shortcuts import redirect, render
 from django.views import View
 from django.views.generic import ListView, DetailView
 
-from .models import Product
+from .models import Product, ProductCategory
 
 
 class ProductListView(ListView):
@@ -11,6 +12,15 @@ class ProductListView(ListView):
     context_object_name = "products"
     ordering = ['-price']
     paginate_by = 6
+
+    def get_queryset(self):
+        query = super(ProductListView, self).get_queryset()
+        query = query.filter(is_active=True)
+        category_name = self.kwargs.get("category")
+        if category_name is not None:
+            query = query.filter(category__url_title__iexact=category_name)
+
+        return query
 
 
 class ProductDetailView(DetailView):
@@ -33,3 +43,14 @@ class ProductFavorite(View):
         product = Product.objects.get(pk=product_id)
         request.session["product_favorite"] = product.id
         return redirect(product.get_absolute_url())
+
+
+def product_categories_components(request: HttpRequest):
+    product_categories_component = ProductCategory.objects.prefetch_related("productcategory_set") \
+        .filter(is_active=True, is_delete=False, parent_id=None)
+
+    context = {
+        "product_categories_component": product_categories_component
+    }
+
+    return render(request, template_name="product_module/components/product_categories_component.html", context=context)
