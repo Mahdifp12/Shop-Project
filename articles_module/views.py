8b44a51d0efd1fc452d02147dc798fa4d5b28170
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-07-05 22:10:04 +0330

    CHANGE & REFACTOR : i'm refactoring this file with PEP

--- articles_module/views.py
+++ articles_module/views.py
@@ -45,7 +45,8 @@ class ArticleDetailView(DetailView):
 
 
 def article_categories_component(request: HttpRequest):
-    article_main_categories = ArticleCategory.objects.filter(is_active=True, parent_id=None)
+    article_main_categories = ArticleCategory.objects.prefetch_related("articlecategory_set")\
+        .filter(is_active=True, parent_id=None)
 
     context = {
         'article_main_categories': article_main_categories,
