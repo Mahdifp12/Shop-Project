62a6c6268759c3350b3874898758146fd598bc77
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-07-05 22:08:57 +0330

    CHANGE & ADD : i'm added a new URL for product category changing

--- product_module/urls.py
+++ product_module/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='product-list'),
+    path('cat/<str:category>', views.ProductListView.as_view(), name="products-by-category-list"),
     path('product-favorite', views.ProductFavorite.as_view(), name='product-favorite'),
     path('<slug:slug>', views.ProductDetailView.as_view(), name='product-detail'),
 ]
