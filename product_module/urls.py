44724055095e1f0ecec5a89cee0be0da111dd7e8
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-07-07 15:44:14 +0330

    CHANGE & ADD : i'm added a new URL path for product brands components

--- product_module/urls.py
+++ product_module/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='product-list'),
     path('cat/<str:category>', views.ProductListView.as_view(), name="products-by-category-list"),
+    path('bra/<str:brand>', views.ProductListView.as_view(), name='product-by-brand-list'),
     path('product-favorite', views.ProductFavorite.as_view(), name='product-favorite'),
     path('<slug:slug>', views.ProductDetailView.as_view(), name='product-detail'),
 ]
